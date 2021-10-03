a7fe939e52cc1fb085cdca561f7e4f74e605c7c2
Diego Andrés <diegoandres_cortes@outlook.com>
2021-10-03 01:39:36 -0500

    move txt string to a file
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Languages/HIFG/.english.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package Hide Images From Guests
+ * @version 2.2
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, Diego Andrés
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+$txt['hifg_cannot_view'] = 'Guests are not allowed to view images in posts, please <a href="' . $scripturl . '?action=signup">Register</a> or <a href="' . $scripturl . '?action=login">Login</a>';
---
+++ Languages/HIFG/.spanish_latin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package Hide Images From Guests
+ * @version 2.2
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, Diego Andrés
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+$txt['hifg_cannot_view'] = 'Los visitantes no pueden visualizar imágenes en los mensajes, por favor <a href="' . $scripturl . '?action=signup">Regístrate</a> o <a href=".' . $scripturl . '?action=login">Inicia Sesión</a>';
